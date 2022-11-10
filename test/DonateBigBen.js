4719ce30c9ad8a890b8fc1489b2c60e3c1b36694
KevinFan <kevinypfan@gmail.com>
2022-11-10 13:09:34 +0800

    full all test

--- test/DonateBigBen.js
+++ test/DonateBigBen.js
@@ -6,10 +6,32 @@ describe("DonateBigBen contract", function () {
     const Token = await ethers.getContractFactory("DonateBigBen");
     const hardhatToken = await Token.deploy();
     let amount = ethers.utils.parseEther("0.001");
-    await expect(hardhatToken.donateMe(amount,{
-      value:amount,
-      gasLimit: 100000,     
-    }))
-    .to.emit(hardhatToken, "BigBenEvent")
+    await expect(
+      hardhatToken.donateMe(amount, {
+        value: amount,
+        gasLimit: 100000,
+      })
+    ).to.emit(hardhatToken, "BigBenEvent");
+  });
+
+  it("it should reverted with Insufficient donate amount", async function () {
+    const Token = await ethers.getContractFactory("DonateBigBen");
+    const hardhatToken = await Token.deploy();
+    let amount = ethers.utils.parseEther("0.0001");
+    await expect(
+      hardhatToken.donateMe(amount, {
+        value: amount,
+        gasLimit: 100000,
+      })
+    ).to.be.revertedWith("Insufficient donate amount");
+  });
+
+  it("it should reverted with Donate failed", async function () {
+    const Token = await ethers.getContractFactory("DonateBigBen");
+    const hardhatToken = await Token.deploy();
+    let amount = ethers.utils.parseEther("0.001");
+    await expect(hardhatToken.donateMe(amount)).to.be.revertedWith(
+      "Donate failed"
+    );
   });
 });
